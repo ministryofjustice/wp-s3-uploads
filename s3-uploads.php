c5d2b18e81c2836607a347798a95c18ac6b23e65
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-02-15 12:04:54 +0000

    WP Cli fix

--- s3-uploads.php
+++ s3-uploads.php
@@ -10,11 +10,12 @@ Author URI: https://hmn.md
 
 require_once __DIR__ . '/inc/namespace.php';
 
-require_once __DIR__ . '/inc/class-wp-cli-command.php';
-require_once __DIR__ . '/inc/class-image-editor-imagick.php';
-require_once __DIR__ . '/inc/class-local-stream-wrapper.php';
-require_once __DIR__ . '/inc/class-plugin.php';
-require_once __DIR__ . '/inc/class-stream-wrapper.php';
-require_once __DIR__ . '/inc/class-wp-cli-command.php';
-
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+
+	require_once __DIR__ . '/inc/class-wp-cli-command.php';
+	
+	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
+}
+
