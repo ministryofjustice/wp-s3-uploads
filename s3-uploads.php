03d3dd1de7c910c914d29dfe8fbc906177f4278f
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-02-15 11:28:53 +0000

    Include Classes

--- s3-uploads.php
+++ s3-uploads.php
@@ -10,4 +10,11 @@ Author URI: https://hmn.md
 
 require_once __DIR__ . '/inc/namespace.php';
 
+require_once __DIR__ . '/inc/class-wp-cli-command.php';
+require_once __DIR__ . '/inc/class-image-editor-imagick.php';
+require_once __DIR__ . '/inc/class-local-stream-wrapper.php';
+require_once __DIR__ . '/inc/class-plugin.php';
+require_once __DIR__ . '/inc/class-stream-wrapper.php';
+require_once __DIR__ . '/inc/class-wp-cli-command.php';
+
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
